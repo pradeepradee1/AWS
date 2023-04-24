d70a396dc1493ce53513b5ce623d1fed5f44591e
pradeepradee1 <pradeepradee1@gmail.com>
2023-04-24 15:24:35 +0530

    Beginner added

---
+++ M1_Beginner/M1_ReadFromS3.py
@@ -0,0 +1,14 @@
+import json
+import boto3
+s3 = boto3.resource('s3')
+
+def lambda_handler(event, context):
+    # TODO implement
+    print(event)
+    print(context)
+    bucket = s3.Bucket('batch1s3bucket')
+    print(bucket.objects.all())
+    print("=========================")
+    for obj in bucket.objects.all():
+        key = obj.key
+        print(key)
